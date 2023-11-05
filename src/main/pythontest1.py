11055d7a0c2aa4e12e13288449baf3c59d27d486
NathanMcCourt <tuk91355@temple.edu>
2023-11-04 20:42:44 -0400

    Added file for proof of concept

---
+++ src/main/pythontest1.py
@@ -0,0 +1,18 @@
+#PB & NM
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Hi everyone!')
+
+def main():
+    print("Hello, world!")
+    
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
